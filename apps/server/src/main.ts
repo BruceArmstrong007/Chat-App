4c98e904c9f157dd8a735f7972f1e21f25a3107d
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 15:14:35 +0530

    Initial Commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -45,6 +45,13 @@ app.use(cors({
  }));
 
 
+ app.use((req, res, next) =>{
+  res.header("Access-Control-Allow-Origin", process.env.WEB_CLIENT_URL);
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+  next();
+});
 
 app.use(cookieParser());
 
