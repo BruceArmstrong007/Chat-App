16720832fa737ec94687b3c8e778d59980e0e307
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 16:18:02 +0530

    Initial Commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,23 +9,23 @@ import { createContext,appRouter } from '@server/trpc';
 import { applyWSSHandler } from '@trpc/server/adapters/ws';
 import * as ws from 'ws';
 
-const wss = new ws.Server({
-  port: 3001,
-});
-const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+// const wss = new ws.Server({
+//   port: 3001,
+// });
+// const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
-wss.on('connection', (ws) => {
-  console.log(`➕➕ Connection (${wss.clients.size})`);
-  ws.once('close', () => {
-    console.log(`➖➖ Connection (${wss.clients.size})`);
-  });
-});
-console.log('✅ WebSocket Server listening on ws://localhost:3001');
-process.on('SIGTERM', () => {
-  console.log('SIGTERM');
-  handler.broadcastReconnectNotification();
-  wss.close();
-});
+// wss.on('connection', (ws) => {
+//   console.log(`➕➕ Connection (${wss.clients.size})`);
+//   ws.once('close', () => {
+//     console.log(`➖➖ Connection (${wss.clients.size})`);
+//   });
+// });
+// console.log('✅ WebSocket Server listening on ws://localhost:3001');
+// process.on('SIGTERM', () => {
+//   console.log('SIGTERM');
+//   handler.broadcastReconnectNotification();
+//   wss.close();
+// });
 
 const app = express();
 
@@ -57,13 +57,13 @@ app.use(cors({
 app.use(cookieParser());
 
 
-// app.use(
-//   ROUTES.API,
-//   trpcExpress.createExpressMiddleware({
-//     router: appRouter,
-//     createContext,
-//   })
-// );
+app.use(
+  ROUTES.API,
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 app.get('/',async(req,res)=>{
   return res.send('Hello');
