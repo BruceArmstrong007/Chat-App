98c789c93d61f2c0d553737b293ada4ca148980f
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 11:23:14 +0530

    Initial Commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -9,23 +9,23 @@ import { createContext,appRouter } from '@server/trpc';
 import { applyWSSHandler } from '@trpc/server/adapters/ws';
 import * as ws from 'ws';
 
-// const wss = new ws.Server({
-//   port: 3001,
-// });
-// const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+const wss = new ws.Server({
+  port: 3001,
+});
+const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
-// wss.on('connection', (ws) => {
-//   console.log(`➕➕ Connection (${wss.clients.size})`);
-//   ws.once('close', () => {
-//     console.log(`➖➖ Connection (${wss.clients.size})`);
-//   });
-// });
-// console.log('✅ WebSocket Server listening on ws://localhost:3001');
-// process.on('SIGTERM', () => {
-//   console.log('SIGTERM');
-//   handler.broadcastReconnectNotification();
-//   wss.close();
-// });
+wss.on('connection', (ws) => {
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.once('close', () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
+});
+console.log('✅ WebSocket Server listening on ws://localhost:3001');
+process.on('SIGTERM', () => {
+  console.log('SIGTERM');
+  handler.broadcastReconnectNotification();
+  wss.close();
+});
 
 const app = express();
 
@@ -45,20 +45,20 @@ app.use(cors({credentials : true , origin :process.env.WEB_CLIENT_URL}));
 app.use(cookieParser());
 
 
-// app.use(
-//   ROUTES.API,
-//   trpcExpress.createExpressMiddleware({
-//     router: appRouter,
-//     createContext,
-//   })
-// );
-app.get('/', async () => {
-  return { hello: 'wait-on 💨' };
+app.use(
+  ROUTES.API,
+  trpcExpress.createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
+app.get('/', async (req,res) => {
+  return res.send('hello');
 });
 
 
 const port = process.env.port || process.env.SERVER_PORT;
 const server = app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}/api`);
+  console.log(`Listening at http://localhost:${port}`);
 });
 server.on('error', console.error);
