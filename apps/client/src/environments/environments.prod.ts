c8457661ba3b95f61de3e4f71fa048da980c2369
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 14:17:35 +0530

    Initial Commit

--- apps/client/src/environments/environments.prod.ts
+++ apps/client/src/environments/environments.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiURL: 'https://chat-app-3lv6.onrender.com/api',
-  wsURL : 'https://chat-app-3lv6.onrender.com'
+  wsURL : 'wss://chat-app-3lv6.onrender.com'
 };
