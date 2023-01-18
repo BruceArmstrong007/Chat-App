2fd89dd17fb2f43606cbc9b53fbef2d621f973ea
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 11:37:21 +0530

    Initial Commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,57 +1,57 @@
 import * as express from 'express';
-import * as path from 'path';
-import Helmet  from 'helmet';
-import * as cors from 'cors';
-import * as cookieParser from 'cookie-parser';
-import * as trpcExpress from '@trpc/server/adapters/express';
-import { createContext,appRouter } from '@server/trpc';
+// import * as path from 'path';
+// import Helmet  from 'helmet';
+// import * as cors from 'cors';
+// import * as cookieParser from 'cookie-parser';
+// import * as trpcExpress from '@trpc/server/adapters/express';
+// import { createContext,appRouter } from '@server/trpc';
 
-import { applyWSSHandler } from '@trpc/server/adapters/ws';
-import * as ws from 'ws';
+// import { applyWSSHandler } from '@trpc/server/adapters/ws';
+// import * as ws from 'ws';
 
-const wss = new ws.Server({
-  port: 3001,
-});
-const handler = applyWSSHandler({ wss, router: appRouter, createContext });
+// const wss = new ws.Server({
+//   port: 3001,
+// });
+// const handler = applyWSSHandler({ wss, router: appRouter, createContext });
 
-wss.on('connection', (ws) => {
-  console.log(`➕➕ Connection (${wss.clients.size})`);
-  ws.once('close', () => {
-    console.log(`➖➖ Connection (${wss.clients.size})`);
-  });
-});
-console.log('✅ WebSocket Server listening on ws://localhost:3001');
-process.on('SIGTERM', () => {
-  console.log('SIGTERM');
-  handler.broadcastReconnectNotification();
-  wss.close();
-});
+// wss.on('connection', (ws) => {
+//   console.log(`➕➕ Connection (${wss.clients.size})`);
+//   ws.once('close', () => {
+//     console.log(`➖➖ Connection (${wss.clients.size})`);
+//   });
+// });
+// console.log('✅ WebSocket Server listening on ws://localhost:3001');
+// process.on('SIGTERM', () => {
+//   console.log('SIGTERM');
+//   handler.broadcastReconnectNotification();
+//   wss.close();
+// });
 
 const app = express();
 
-const ROUTES = {
-  ASSETS: '/assets',
-  API: '/api',
-};
+// const ROUTES = {
+//   ASSETS: '/assets',
+//   API: '/api',
+// };
 
-app.use(Helmet());
+// app.use(Helmet());
 
-app.use(ROUTES.ASSETS, express.static(path.join(__dirname, 'assets')));
+// app.use(ROUTES.ASSETS, express.static(path.join(__dirname, 'assets')));
 
-app.use(cors({credentials : true , origin :process.env.WEB_CLIENT_URL}));
-
-
-
-app.use(cookieParser());
+// app.use(cors({credentials : true , origin :process.env.WEB_CLIENT_URL}));
 
 
-app.use(
-  ROUTES.API,
-  trpcExpress.createExpressMiddleware({
-    router: appRouter,
-    createContext,
-  })
-);
+
+// app.use(cookieParser());
+
+
+// app.use(
+//   ROUTES.API,
+//   trpcExpress.createExpressMiddleware({
+//     router: appRouter,
+//     createContext,
+//   })
+// );
 app.get('/', async (req,res) => {
   return res.send('hello');
 });
