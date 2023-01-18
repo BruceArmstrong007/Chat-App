8e7028612797fbe89aadf9b2cb394d1552095835
Bruce Armstrong <brucearmstrongofficial@gmail.com>
2023-01-18 15:53:31 +0530

    Initial Commit

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -46,7 +46,7 @@ app.use(cors({
  app.use(Helmet());
 
  app.use((req, res, next) =>{
-  res.header("Access-Control-Allow-Origin", process.env.WEB_CLIENT_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
   res.header(
